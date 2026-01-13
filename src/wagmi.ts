6a289e7e5595bbef0ff55c5ff110e5e53fa1495d
ธณวัฒน์ ประวันตา <pouaun2499@gmail.com>
2026-01-13 15:08:41 +0700

    Updated wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -19,7 +19,7 @@ export const ritualchain = defineChain({
 export const config = getDefaultConfig({
   appName: "MeeChain",
   projectId: "b0d81328f8ab0541fdede7db9ff25cb1",
-  chains: [ritualchain, mainnet, sepolia, base, optimism, arbitrum, polygon, bsc ],
+  chains: [ritualchain, mainnet, sepolia, base, optimism, arbitrum, polygon ],
   transports: {
     [ritualchain.id]: http(undefined, {
         batch: {
